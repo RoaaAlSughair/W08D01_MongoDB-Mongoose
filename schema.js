c78c84c5e609a3de0713d86bd9d4d3ac6937ac8c
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-05-16 23:51:42 +0300

    Pulse check question 3 + 4

--- schema.js
+++ schema.js
@@ -1,1 +1,10 @@
 const mongoose = require("mongoose");
+const todoListSchema = new mongoose.Schema({
+    task: {type:String, required:true, unique:true},
+    description: {type:String},
+    deadline: {type:Date, default:Date.now, required:true},
+    isCompleted: {type:Boolean, required:true},
+    priority: {type:String},
+});
+
+module.exports = mongoose.model("Todo list", todoListSchema);
