8a47af079623769d016d0b6ceab400a15480ea5e
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-05-17 00:14:29 +0300

    What I could solve of practice

--- app.js
+++ app.js
@@ -5,12 +5,49 @@ const todoModel = require("./schema");
 const app = express();
 app.use(express.json());
 
-app.get("/todos", (req, res) => {});
-app.post("/create/todo", (req, res) => {});
-app.put("/update/todo", (req, res) => {});
-app.delete("/delete/todo", (req, res) => {});
+app.get("/todos", (req, res) => {
+  todoModel
+    .find({})
+    .then((result) => {
+      res.status(200);
+      res.json(result);
+    })
+    .catch((error) => {
+      res.json(error);
+    });
+});
+app.post("/create/todo", (req, res) => {
+  const { task, description, deadline, isCompleted, priority } = req.body;
+
+  const todo = new todoModel({
+    task,
+    description,
+    deadline,
+    isCompleted,
+    priority,
+  });
+
+  todo
+    .save()
+    .then((result) => {
+      res.status(201);
+      res.json(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+app.put("/update/todo", (req, res) => {
+  const updatedTodo = req.body;
+  res.status(200);
+  res.json("Put method");
+});
+app.delete("/delete/todo", (req, res) => {
+  res.status(200);
+  res.json("Delete method");
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Example app is listening at http://localhost:${PORT}`);
+  console.log(`Example app is listening at http://localhost:${PORT}`);
 });
